482ef75e3977542e526700d7a05fc557dc6f04d6
ojs <contact@ollyjsmith.com>
2019-09-21 11:46:02 +0100

    add autoscrolling to credits

--- src/modules/CreditsStage.js
+++ src/modules/CreditsStage.js
@@ -4,17 +4,43 @@ let CreditsStage = function( _$ele ){
   this.$credits_contents = this.$credits.querySelector('.screen--content');
   this.$progress = this.$ele.querySelector('.progress--indicator');
   this.progress = 0;
+  this.isAuto = true;
+  this.autoScrollStep = 1;
+  this.timer;
+  this.current = {
+    type: 'credits'
+  };
 
-  this.$credits.addEventListener('scroll', () => {
+  this.$credits.scrollTop = 0;
+
+  this.setAuto = () => {
+    this.isAuto = true;
+    clearInterval( this.timer );
+    this.timer = setInterval( () => {
+      if( this.isAuto ){
+        this.$credits.scrollTop += this.autoScrollStep;
+      }
+    }, 100 );
+  };
+
+  this.stopAuto = () => {
+    this.isAuto = false;
+    clearInterval( this.timer );
+  }
+
+  this.$credits.addEventListener('scroll', (e) => {
     this.progress = this.$credits.scrollTop / (this.$credits.scrollHeight - this.$credits.offsetHeight);
     this.update();
   })
 
   this.activate = () => {
+    this.$credits.scrollTop = 0;
+    this.setAuto();
     this.$credits.classList.add('active');
     this.$ele.classList.add('active');
   }
   this.deactivate = () => {
+    this.stopAuto();
     this.$credits.classList.remove('active');
     this.$ele.classList.remove('active');
   }
